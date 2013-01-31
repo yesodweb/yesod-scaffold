6619e7a5bb40fdde3990e7a1350fd82541c4c309
paul-rouse <pgr@doynton.org>
2013-01-31 14:21:36 +0000

    Fix tests subdirectory for ResourceT in Persistent types, and add example.

--- tests/HomeTest.hs
+++ tests/HomeTest.hs
@@ -4,10 +4,12 @@ module HomeTest
     ) where
 
 import TestImport
+import qualified Data.List as L
 
 homeSpecs :: Specs
 homeSpecs =
-  describe "These are some example tests" $
+  describe "These are some example tests" $ do
+
     it "loads the index and checks it looks right" $ do
       get_ "/"
       statusIs 200
@@ -22,3 +24,12 @@ homeSpecs =
       htmlCount ".message" 1
       htmlAllContain ".message" "Some Content"
       htmlAllContain ".message" "text/plain"
+
+    -- This is a simple example of using a database access in a test.  The
+    -- test will succeed for a fresh scaffolded site with an empty database,
+    -- but will fail on an existing database with a non-empty user table.
+    it "leaves the user table empty" $ do
+      get_ "/"
+      statusIs 200
+      users <- runDB $ selectList ([] :: [Filter User]) []
+      assertEqual "user table empty" 0 $ L.length users
