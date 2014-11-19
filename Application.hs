0057657161fb26252873941bff80fce0e95920d1
Michael Snoyman <michael@snoyman.com>
2014-11-19 13:46:58 +0200

    Handler.Common

--- Application.hs
+++ Application.hs
@@ -9,7 +9,6 @@ import Import
 import Yesod.Auth
 import Yesod.Default.Config
 import Yesod.Default.Main
-import Yesod.Default.Handlers
 import Network.Wai.Middleware.RequestLogger
     ( mkRequestLogger, outputFormat, OutputFormat (..), IPAddrSource (..), destination
     )
@@ -26,6 +25,7 @@ import Yesod.Core.Types (loggerSet, Logger (Logger))
 
 -- Import all relevant handler modules here.
 -- Don't forget to add new modules to your cabal file!
+import Handler.Common
 import Handler.Home
 
 -- This line actually creates our YesodDispatch instance. It is the second half
