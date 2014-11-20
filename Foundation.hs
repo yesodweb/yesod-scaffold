383bf7cdc791f3cc79f7bf59bcf08f72319a9526
Michael Snoyman <michael@snoyman.com>
2014-11-20 08:14:46 +0200

    redirectToReferer to True

--- Foundation.hs
+++ Foundation.hs
@@ -124,6 +124,8 @@ instance YesodAuth App where
     loginDest _ = HomeR
     -- Where to send a user after logout
     logoutDest _ = HomeR
+    -- Override the above two destinations when a Referer: header is present
+    redirectToReferer _ = True
 
     getAuthId creds = runDB $ do
         x <- getBy $ UniqueUser $ credsIdent creds
