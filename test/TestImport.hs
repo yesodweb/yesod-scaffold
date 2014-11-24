1ec8eb2e02a56baaeb887ea44fdc03eb825e4398
Michael Snoyman <michael@snoyman.com>
2014-11-24 09:55:15 +0200

    stylish-haskell and import cleanup

--- test/TestImport.hs
+++ test/TestImport.hs
@@ -3,16 +3,15 @@ module TestImport
     , module X
     ) where
 
-import Yesod.Test as X
-import Database.Persist as X hiding (get)
-import Database.Persist.Sql (SqlPersistM, runSqlPersistMPool)
-import ClassyPrelude as X
-import Application (makeFoundation)
-import Yesod.Default.Config2 (loadAppSettings, ignoreEnv)
-import Test.Hspec as X
-
-import Foundation as X
-import Model as X
+import Application           (makeFoundation)
+import ClassyPrelude         as X
+import Database.Persist      as X hiding (get)
+import Database.Persist.Sql  (SqlPersistM, runSqlPersistMPool)
+import Foundation            as X
+import Model                 as X
+import Test.Hspec            as X
+import Yesod.Default.Config2 (ignoreEnv, loadAppSettings)
+import Yesod.Test            as X
 
 runDB :: SqlPersistM a -> YesodExample App a
 runDB query = do
