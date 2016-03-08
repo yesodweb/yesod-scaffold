ec97b5448090477011821983a31c261a8253d767
Michael Snoyman <michael@snoyman.com>
2016-03-08 13:53:36 +0200

    build takes --no-run-tests

--- build.hs
+++ build.hs
@@ -8,6 +8,11 @@ import Shared
 
 main :: IO ()
 main = shelly $ do
+    args <- getArgs
+    let noRunTests = "--no-run-tests" `elem` args
+        testArgs
+            | noRunTests = ["test", "--no-run-tests"]
+            | otherwise = ["test"]
     rm_rf "yesod-scaffold"
     run_ "git" ["clone", ".", "yesod-scaffold"]
     cd "yesod-scaffold"
@@ -16,7 +21,7 @@ main = shelly $ do
         unless (branch == master) $ run_ "git" ["merge", master]
         run_ "git" ["diff", "--exit-code"]
         run_ "stack" ["init"]
-        run_ "stack" ["test"] -- sometimes useful, e.g. no local database: "--no-run-tests"
+        run_ "stack" testArgs
         run_ "packdeps" ["PROJECTNAME.cabal"]
         run_ "git" ["clean", "-fxd"]
         createHsFiles "yesod-scaffold" branch $ "hsfiles" </> unpack ("yesod-" ++ branch) <.> "hsfiles"
