b5693b0a6316fcadc8f2ef8b751ba375386a338f
Michael Snoyman <michael@snoyman.com>
2016-09-07 14:09:13 +0300

    Revert "Merge branch 'postgres' into minimal"
    This reverts commit fa16da40f38a20dabc9fb80fc92da44e96996711, reversing
    changes made to 5876e9fd8c9905c7ce0e70724a1c736984cb4624.

--- Application.hs
+++ Application.hs
@@ -8,16 +8,7 @@ module Application where
 import Foundation
 import Yesod.Core
 
-<<<<<<< HEAD
 import Add
 import Home
-=======
--- Import all relevant handler modules here.
--- Don't forget to add new modules to your cabal file!
-import Handler.Common
-import Handler.Home
-import Handler.Comment
-import Handler.Profile
->>>>>>> postgres
 
 mkYesodDispatch "App" resourcesApp
