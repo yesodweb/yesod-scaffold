25d6337179ad83069da75c34e94001861500dbf8
Michael Snoyman <michael@snoyman.com>
2016-09-07 15:04:31 +0300

    Fix non-exhaustive pattern match

--- Foundation.hs
+++ Foundation.hs
@@ -95,6 +95,7 @@ instance Yesod App where
     isAuthorized FaviconR _ = return Authorized
     isAuthorized RobotsR _ = return Authorized
     isAuthorized (StaticR _) _ = return Authorized
+    isAuthorized (FaySiteR _) _ = return Authorized
 
     isAuthorized ProfileR _ = isAuthenticated
 
