158ef670b8b8cfbc8c4b658dfda915837d2459dc
Amitaibu <amitai@gizra.com>
2016-09-15 13:14:53 +0300

    Fix assert text

--- test/Handler/HomeSpec.hs
+++ test/Handler/HomeSpec.hs
@@ -9,7 +9,7 @@ spec = withApp $ do
         it "loads the index and checks it looks right" $ do
           get HomeR
           statusIs 200
-          htmlAnyContain "h1" "Yesod; A modern framework for blazing fast websites"
+          htmlAnyContain "h1" "a modern framework for blazing fast websites"
 
           request $ do
               setMethod "POST"
