55d0c103f92cf953fea826af24def58decb8bade
Michael Snoyman <michael@snoyman.com>
2017-02-06 10:31:19 +0200

    WIP

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
 module Foundation where
 
 import Import.NoFoundation
--- src/Settings.hs
+++ src/Settings.hs
@@ -1,4 +1,8 @@
-{-# Language CPP #-}
+{-# LANGUAGE CPP               #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE TemplateHaskell   #-}
 -- | Settings are centralized, as much as possible, into this file. This
 -- includes database connection settings, static file locations, etc.
 -- In addition, you can configure a number of different aspects of Yesod
@@ -6,18 +10,19 @@
 -- declared in the Foundation.hs file.
 module Settings where
 
-import ClassyPrelude.Yesod
-import qualified Control.Exception as Exception
-import Data.Aeson                  (Result (..), fromJSON, withObject, (.!=),
-                                    (.:?))
-import Data.FileEmbed              (embedFile)
-import Data.Yaml                   (decodeEither')
-import Database.Persist.Postgresql (PostgresConf)
-import Language.Haskell.TH.Syntax  (Exp, Name, Q)
-import Network.Wai.Handler.Warp    (HostPreference)
-import Yesod.Default.Config2       (applyEnvValue, configSettingsYml)
-import Yesod.Default.Util          (WidgetFileSettings, widgetFileNoReload,
-                                    widgetFileReload)
+import           ClassyPrelude.Yesod
+import qualified Control.Exception           as Exception
+import           Data.Aeson                  (Result (..), fromJSON, withObject,
+                                              (.!=), (.:?))
+import           Data.FileEmbed              (embedFile)
+import           Data.Yaml                   (decodeEither')
+import           Database.Persist.Postgresql (PostgresConf)
+import           Language.Haskell.TH.Syntax  (Exp, Name, Q)
+import           Network.Wai.Handler.Warp    (HostPreference)
+import           Yesod.Default.Config2       (applyEnvValue, configSettingsYml)
+import           Yesod.Default.Util          (WidgetFileSettings,
+                                              widgetFileNoReload,
+                                              widgetFileReload)
 
 -- | Runtime settings to configure this application. These settings can be
 -- loaded from various sources: defaults, environment variables, config files,
@@ -62,7 +67,7 @@ data AppSettings = AppSettings
 instance FromJSON AppSettings where
     parseJSON = withObject "AppSettings" $ \o -> do
         let defaultDev =
-#if DEVELOPMENT
+#ifdef DEVELOPMENT
                 True
 #else
                 False
