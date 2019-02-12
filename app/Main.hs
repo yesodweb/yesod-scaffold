9db7c5a9f488c37331fbbcf0a70a65c5654f1e3a
Michael Snoyman <michael@snoyman.com>
2019-02-12 11:11:01 +0200

    Disable mongo for now (not in LTS)

--- app/Main.hs
+++ app/Main.hs
@@ -109,7 +109,7 @@ pull = do
   proc "git" ["checkout", "master"] runProcess_
 
 branches :: [Text]
-branches = [ "postgres", "sqlite", "mysql", "mongo", "simple" -- , "postgres-fay"
+branches = [ "postgres", "sqlite", "mysql", "simple" -- , "mongo", "postgres-fay"
            , "minimal"
            ]
 
