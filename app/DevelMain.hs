5fefa26175ad5c67901187cb58f222c5bc22394b
Andreas Schacker <menelaos@users.noreply.github.com>
2026-03-12 18:11:12 +0100

    Fix syntax in Haddock comment

--- app/DevelMain.hs
+++ app/DevelMain.hs
@@ -6,7 +6,7 @@
 --
 -- 1. Start up GHCi
 --
--- $ stack ghci PROJECTNAME:lib --no-load --work-dir .stack-work-devel
+-- > $ stack ghci PROJECTNAME:lib --no-load --work-dir .stack-work-devel
 --
 -- 2. Load this module
 --
